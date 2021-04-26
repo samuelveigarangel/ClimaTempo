848125fc20a0636101ebfdf3e997468ca2179861
Samuel Veiga Rangel <samuelveigarangel@hotmail.com>
2021-04-26 15:12:24 -0300

    Updade
    V0.1

---
+++ ClimaTempo/clima/__init__.py
@@ -0,0 +1,39 @@
+import requests, json
+
+
+class Clima:
+
+
+    def __init__(self, token, city):
+        self.token = token
+        self.city = city
+
+    #registrar cidade para utilizar algumas consultas
+    def register_city(self, id_city):
+        """'http://apiadvisor.climatempo.com.br/api-manager/user-token/:5cca58af8cc349c15c55c1596219c717/locales' \
+                -H 'Content-Type: application/x-www-form-urlencoded' \
+                -d 'localeId[]='"""
+        url = 'http://apiadvisor.climatempo.com.br/api-manager/user-token/5cca58af8cc349c15c55c1596219c717/locales'
+        headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+        local = 'localeId[]=' + str(id_city)
+        response = requests.put(url, headers=headers, data=local)
+        print(json.loads(response.text))
+
+
+    ##obten id cidade
+    def data_city(self):
+        #city = input('Informe o nome da cidade: ')
+        url = 'http://apiadvisor.climatempo.com.br/api/v1/locale/city?name=' + self.city + '&state=ce&token=' + self.token
+        response = requests.get(url)
+        date = json.loads(response.text)
+        return date[0]['id']
+
+    def clima_city(self):
+        url = 'http://apiadvisor.climatempo.com.br/api/v1/weather/locale/8050/current?token=' + self.token
+        response = requests.get(url)
+        info_city = json.loads(response.text)        
+        print(f'Estado: {info_city["state"]}')
+        print(f'Cidade: {info_city["name"]}')
+        print(f'País: {info_city["country"]}')
+        print(f'Temperatura: {info_city["data"]["temperature"]}C graus com sensação termica de {info_city["data"]["sensation"]}C graus')
+        print(f'Condição: {info_city["data"]["condition"]}')
---
+++ ClimaTempo/main.py
@@ -0,0 +1,8 @@
+from clima import *
+
+
+a = Clima('5cca58af8cc349c15c55c1596219c717', 'fortaleza')
+#obtem id da cidade para registro (1 cidade por dia)
+#id_city = a.data_city()
+a.clima_city()
+#a.register_city(id_city)
